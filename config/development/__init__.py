c915b44b63201a2f8553a7431822e2903fea9a5e
David Skoda <dskoda1@binghamton.edu>
2017-01-25 20:15:53 -0500

    change to postgres, add travis

--- config/development/__init__.py
+++ config/development/__init__.py
@@ -3,6 +3,6 @@ import os
 DEBUG = True
 SECRET_KEY = 'my precious'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/supervisor_watcher')
 HOST = 'localhost'
 PORT = int(os.environ.get('PORT', 5000))
